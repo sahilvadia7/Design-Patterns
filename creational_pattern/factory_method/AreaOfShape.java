c0f02372cce5c59d59d207c27d42761cabf3bcc7
sahil <sahilvadia7776@gmail.com>
2024-07-22 22:46:02 +0530

    creationalPattern

---
+++ creational_pattern/factory_method/AreaOfShape.java
@@ -0,0 +1,16 @@
+package creational_pattern.factory_method;
+
+interface Shape{
+    void Area();
+}
+
+class circle implements Shape{
+
+    public void Area(){
+
+    }
+}
+
+public class AreaOfShape {
+    
+}
